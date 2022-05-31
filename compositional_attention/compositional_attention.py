f82987f50c6e4b8602e47072c90dec098909276c
Rishit Dagli <rishit.dagli@gmail.com>
2022-05-31 13:10:30 +0000

    Einops on multiple tensors

---
+++ compositional_attention/compositional_attention.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+from functools import wraps
+
+
+def _many(fn):
+    @wraps(fn)
+    def inner(tensors, pattern, **kwargs):
+        return (fn(tensor, pattern, **kwargs) for tensor in tensors)
+
+    return inner
+
+
