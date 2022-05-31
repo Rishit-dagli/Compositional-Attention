6cb8d1ced59f4a84b078fcf6ff1d1ada9739c0b0
Rishit Dagli <rishit.dagli@gmail.com>
2022-05-31 13:12:18 +0000

    Init Compositional Attention class

--- compositional_attention/compositional_attention.py
+++ compositional_attention/compositional_attention.py
@@ -12,5 +12,16 @@ def _many(fn):
 
 rearrange_many = _many(rearrange)
 
-
-
+class CompositionalAttention(tf.keras.layers.Layer):
+    def __init__(
+        self,
+        dim,
+        dim_head=64,
+        num_searches=8,
+        num_retrievals=2,
+        dropout=0.0,
+        prenorm=False,
+        causal=False,
+        **kwargs
+    ):
+        super(CompositionalAttention, self).__init__(**kwargs)
